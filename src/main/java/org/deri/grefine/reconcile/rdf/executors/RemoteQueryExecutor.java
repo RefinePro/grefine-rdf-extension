01046f8d47425f859512ee190eaf857acb603e49
Ernesto <ernesto@refinepro.com>
2021-06-08 15:01:33 -0400

    Add integration for OpenRefine Authenticator extension

--- src/main/java/org/deri/grefine/reconcile/rdf/executors/RemoteQueryExecutor.java
+++ src/main/java/org/deri/grefine/reconcile/rdf/executors/RemoteQueryExecutor.java
@@ -7,6 +7,13 @@ import java.io.IOException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonGenerationException;
 
+import org.shaded.apache.http.auth.AuthScope;
+import org.shaded.apache.http.auth.Credentials;
+import org.shaded.apache.http.auth.UsernamePasswordCredentials;
+import org.shaded.apache.http.client.CredentialsProvider;
+import org.shaded.apache.http.client.HttpClient;
+import org.shaded.apache.http.impl.client.BasicCredentialsProvider;
+import org.shaded.apache.http.impl.client.HttpClients;
 import org.shaded.apache.jena.query.QueryExecution;
 import org.shaded.apache.jena.query.QueryExecutionFactory;
 import org.shaded.apache.jena.query.ResultSet;
@@ -16,51 +23,56 @@ import org.shaded.apache.jena.query.ResultSetFactory;
  * @author fadmaa
  * query a remote SPARQL endpoint
  */
-public class RemoteQueryExecutor implements QueryExecutor{
-	protected String sparqlEndpointUrl;
-	protected String defaultGraphUri;
-	
-	public RemoteQueryExecutor(String sparqlEndpointUrl, String defaultGraphUri) {
-		this.sparqlEndpointUrl = sparqlEndpointUrl;
-		this.defaultGraphUri = defaultGraphUri;
-	}
+public class RemoteQueryExecutor implements QueryExecutor {
+    protected String sparqlEndpointUrl;
+    protected String defaultGraphUri;
 
-	@Override
-	public ResultSet sparql(String sparql) {
-		QueryExecution qExec;
-		if(defaultGraphUri==null){
-			qExec = QueryExecutionFactory.sparqlService(sparqlEndpointUrl, sparql);
-		}else{
-			qExec = QueryExecutionFactory.sparqlService(sparqlEndpointUrl, sparql, defaultGraphUri);
-		}
-		ResultSet results = null;
-		try {
-			ResultSet res = qExec.execSelect();
-			results = ResultSetFactory.copyResults(res);
-		} finally {
-			qExec.close();
-		}
-		return results;
-	}
+    public RemoteQueryExecutor(String sparqlEndpointUrl, String defaultGraphUri) {
+        this.sparqlEndpointUrl = sparqlEndpointUrl;
+        this.defaultGraphUri = defaultGraphUri;
+    }
 
-	@Override
-	public void save(String serviceId, FileOutputStream baseDir) throws IOException {
-		//nothing to save... all data is external
-	}
-	
-	@Override
-	public void write(JsonGenerator writer) throws JsonGenerationException, IOException {
-		writer.writeStartObject();
-		writer.writeStringField("type", "remote");
-		writer.writeStringField("sparql-url", sparqlEndpointUrl);
-		if(defaultGraphUri!=null){
-			writer.writeStringField("default-graph-uri", defaultGraphUri);
-		}
-		writer.writeEndObject();
-	}
+    @Override
+    public ResultSet sparql(String sparql) {
+        QueryExecution qExec;
+        CredentialsProvider credProvider = new BasicCredentialsProvider();
+        Credentials credentials = new UsernamePasswordCredentials(System.getProperty("OR_AUTH_USERNAME"), System.getProperty("OR_AUTH_PASSWORD"));
+        credProvider.setCredentials(AuthScope.ANY, credentials);
+        HttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credProvider)
+                .build();
+        if (defaultGraphUri == null) {
+            qExec = QueryExecutionFactory.sparqlService(sparqlEndpointUrl, sparql, httpclient);
+        } else {
+            qExec = QueryExecutionFactory.sparqlService(sparqlEndpointUrl, sparql, defaultGraphUri, httpclient);
+        }
+        ResultSet results = null;
+        try {
+            ResultSet res = qExec.execSelect();
+            results = ResultSetFactory.copyResults(res);
+        } finally {
+            qExec.close();
+        }
+        return results;
+    }
 
-	@Override
-	public void initialize(FileInputStream in) {
-		//nothing to initialize
-	}
+    @Override
+    public void save(String serviceId, FileOutputStream baseDir) throws IOException {
+        //nothing to save... all data is external
+    }
+
+    @Override
+    public void write(JsonGenerator writer) throws JsonGenerationException, IOException {
+        writer.writeStartObject();
+        writer.writeStringField("type", "remote");
+        writer.writeStringField("sparql-url", sparqlEndpointUrl);
+        if (defaultGraphUri != null) {
+            writer.writeStringField("default-graph-uri", defaultGraphUri);
+        }
+        writer.writeEndObject();
+    }
+
+    @Override
+    public void initialize(FileInputStream in) {
+        //nothing to initialize
+    }
 }
